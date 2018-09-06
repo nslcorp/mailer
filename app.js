b0927a0be7b32d84764e09a129cbc3b549451664
Serhii_Nikitin <Vverbatim123>
2018-09-06 18:20:41 +0300

    second changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ here: "answer" });
+  res.send({ here: "answer second" });
 });
 
 const PORT = process.env.PORT || 5000;
