b039d597cca045276dba6c69a26eb08233ae89ac
Serhii_Nikitin <Vverbatim123>
2018-09-06 18:05:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ here: "answer" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("App run on :" + PORT));
