fca813cd1a56c79ced747f61e8fa4080af067fe9
Serhii_Nikitin <Vverbatim123>
2018-09-07 16:43:56 +0300

    add routes, passport auth

---
+++ services/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const User = require('../models/User').User;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(user => {
+        if (user) {
+          done(null, user);
+        } else {
+          new User({ googleId: profile.id }).save().then(user => done(null, user));
+        }
+      });
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+    }
+  )
+);
